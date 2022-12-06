a35dfb92fd8440902da518bc068edd8bb89c9930
Ilya Perederey <ilya_soldat@mail.ru>
2022-12-06 20:47:01 +0300

    DZ2.3

---
+++ DZseminar2.3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.WriteLine("введите число");
+int n = Convert.ToInt32 (Console.ReadLine());
+string str = n.ToString();
+ if (n < 99)
+ { 
+   Console.WriteLine("третьей цифры нет"); 
+ }
+ else
+ Console.WriteLine( str [2]);
